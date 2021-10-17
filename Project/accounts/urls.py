2067b66d474ab920d7088a0d3f2ab0b2f008bfad
gHo0sT <nsakib800@gmail.com>
2021-10-18 00:30:18 +0600

    logout added

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.user_login, name='login'),
-    path('signup', views.user_registration, name='signup')
+    path('signup', views.user_registration, name='signup'),
+    path('logout', views.user_logout, name='logout'),
 ]
--- Project/accounts/views.py
+++ Project/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == 'GET':
@@ -49,3 +50,9 @@ def user_registration(request):
 
         else:
             return render(request, 'accounts/signup.html', {'error':'Please Fill Up all the Information'})
+
+
+@login_required(login_url='login')
+def user_logout(request):
+    auth.logout(request)
+    return redirect('home')
