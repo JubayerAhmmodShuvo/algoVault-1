81f84d5af5d9a4ddcb80076896f969c7958fde90
gHo0sT <nsakib800@gmail.com>
2021-12-28 14:26:55 +0600

    All authentication features created

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('login', views.user_login, name='login'),
     path('signup', views.user_registration, name='signup'),
     path('logout', views.user_logout, name='logout'),
+    path('emailsent', views.email_sent, name='emailsent'),
+    path('verify/<auth_token>', views.verify, name='verify'),
+    path('resetpass', views.reset_password, name='reset_password'),
+    path('reset/<auth_token>', views.verify_new_password_token, name='reset_verify'),
+    path('newpass/<username>', views.set_new_password, name='new_password')
 ]
--- Project/main_app/models.py
+++ Project/main_app/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, max_length=256)
     fullName = models.CharField(max_length=50, default="N/A")
     isVarified = models.BooleanField(default=False)
+    auth_token = models.CharField(null=True, max_length=200)
     isAdmin = models.BooleanField(default=False)
 
     def __str__(self):
